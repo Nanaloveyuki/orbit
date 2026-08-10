ffb35928dee94f4ff68479428d61ebd4261b4e58
Nanaloveyuki <miaomiao_159@outlook.com>
2026-08-10 08:22:18 +0800

    chore: update moonview to beta.6 (#35)

--- orbit-cli/src/cli.mjs
+++ orbit-cli/src/cli.mjs
@@ -23,7 +23,7 @@ const webviewInstallModes = new Set([
 export const compatibilityProfile = Object.freeze({
   orbit: "0.1.0-alpha.1",
   orby: "0.1.0-beta.3",
-  moonview: "0.1.0-beta.5",
+  moonview: "0.1.0-beta.6",
   plugin_abi: 2,
   plugin_sidecar_schema: 2,
   configuration_schema: 2,
